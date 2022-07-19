9a9173c0efced6a6dbadc616534440f7b36b18f1
bigdaddypenguin <rpclancyunc@gmail.com>
2022-07-19 01:32:54 -0400

    database updates

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var express = require('express'),
 var app = express();
 var crypto = require("crypto");
 var consumerSecretApp = '2BD3AFE25F9F53B0D583B8BE7099E1057A1ACA46B736A8AF58AB1E62E3C732D8';
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: "postgres://gthzmrkcscclfm:9f798a485fed64de9bcc5f360b541f8bcfa24e96b7f34f149c02f46c38c6fc95@ec2-54-87-179-4.compute-1.amazonaws.com:5432/d88p9db3gss7c3",
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 console.log('consumer secret - '+consumerSecretApp);
 
@@ -34,6 +42,16 @@ app.post('/', function (req, res) {
         console.log(envelope);
         console.log(JSON.stringify(envelope) );
         console.log('recordId: ' + envelope.context.environment.parameters.recordId);
+        pool.query(`SELECT * FROM t_inventory;`, (err, res) =>{
+          if (err){
+            console.log("couldn't get data");
+            console.log(err);
+          }
+          else{
+            console.log(res.rows);
+          }
+        
+        });
         res.render('index', { title: envelope.context.user.userName, req : JSON.stringify(envelope), 
             recordId : envelope.context.environment.parameters.recordId });
     }else{
