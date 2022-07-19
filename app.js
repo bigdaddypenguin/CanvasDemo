4b1e4d793e1f92b425cc167a811c9f63fe1498f8
bigdaddypenguin <rpclancyunc@gmail.com>
2022-07-19 02:03:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ app.post('/', function (req, res) {
           }
           else{
             console.log(res.rows);
-            db_result = res.rows;
+            db_result = res;
           }
         
         });
         res.render('index', { title: envelope.context.user.userName, req : JSON.stringify(envelope), 
-            recordId : envelope.context.environment.parameters.recordId, inventory: JSON.stringify(res) });
+            recordId : envelope.context.environment.parameters.recordId, inventory: JSON.stringify(db_result) });
     }else{
         res.send("authentication failed");
     } 
